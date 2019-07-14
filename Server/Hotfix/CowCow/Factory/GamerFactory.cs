fcf659ba6290dd98a92f44ac5d731721d9b90c78
Unknown <510264505@qq.com>
2019-07-14 23:34:43 +0800

    创建工厂

---
+++ Server/Hotfix/CowCow/Factory/GamerFactory.cs
@@ -0,0 +1,14 @@
+﻿using ETModel;
+
+namespace ETHotfix
+{
+    public static class GamerFactory
+    {
+        public static Gamer Create(long actorId, long userId, long ? id = null)
+        {
+            Gamer gamer = ComponentFactory.CreateWithId<Gamer, long>(id ?? IdGenerater.GenerateId(), userId);
+            gamer.ActorID = actorId;
+            return gamer;
+        }
+    }
+}
