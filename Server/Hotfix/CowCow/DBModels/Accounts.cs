7b159421c3cadbd108d5efc7b09b0323cadd091a
Unknown <510264505@qq.com>
2019-07-14 23:33:24 +0800

    修改登录接口

---
+++ Server/Hotfix/CowCow/DBModels/Accounts.cs
@@ -0,0 +1,12 @@
+﻿using ETModel;
+using System;
+
+namespace ETHotfix
+{
+    public class Accounts : Entity
+    {
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public DateTime LoginTime { get; set; }
+    }
+}
